d80a7a375a0dadc62d346f8535c107634a709b4b
Tomáš Ondráček <t.ondracek187@gmail.com>
2025-04-28 18:58:22 +0200

    fixed variable name overshadowing

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -66,5 +66,5 @@ if __name__ == '__main__':
         load_yaml(input("File name: "))
         print("Executed -ls on current folder")
     elif choice == "4":
-        password = input("Enter master password: ")
-        authenticate(password)
+        passwordInput = input("Enter master password: ")
+        authenticate(passwordInput)
